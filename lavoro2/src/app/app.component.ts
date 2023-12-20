2e70e51ea05669e194be82d888ca4b2d7d3fa2d0
Matteo Chiroli <chiroli.matteo@iisgalvanimi.edu.it>
2023-12-20 08:40:52 +0000

    esercizio *ng For

---
+++ lavoro2/src/app/app.component.ts
@@ -0,0 +1,11 @@
+// app.component.ts
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  items: string[] = ['Item 1', 'Item 2', 'Item 3'];
+}
